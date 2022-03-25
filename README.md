a301defdf3d8b937d592d2ef19e8c9fd3735aa96
mawilson1234 <michael.a.wilson@yale.edu>
2022-03-25 11:28:43 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Classify Random
+
+Examines the performance of linear support vector machines (SVMs) on classifying randomly generated and assigned data.
+
+## Installation
+
+Clone this git repo and run the following to install the required (non-standard) dependencies:
+
+  - `pip install hydra-core --upgrade`  
+  - `pip3 install numpy tqdm omegaconf scipy sklearn`
+
+## Usage
+
+From the terminal run:
+  - `python3 classify-random.py ntrials=1000 ngroups=2 ntrain=1000 ntest=100 ndims=10`
+  
+This will run 1000 trials where a linear SVM is fit on 1000 randomly initialized 10-dimensional vectors which are randomly assigned to two equal-size groups. The SVM will do its best to classify the random embeddings according to the random labels for the training data. Then, 100 randomly initialized 10-dimensional vectors will be generated as a test set. Mean accuracy and standard error are logged every 50 iterations for train and test performance separately.
+
+You can adjust the settings above by filling in different values for the numbers after the `=`. Here's an explanation of what each option does.
+
+  - `ntrials`: The number of SVMs to fit.
+  - `ngroups`: The number of groups to randomly assign equal numbers of generated embeddings to.
+  - `ntrain`: The number of embeddings in each training set. Must be a multiple of `ngroups` (because equal numbers of embeddings are assigned to each group).
+  - `ntest`: The number of embeddings in each test set. Must be a multiple of `ngroups` (because equal numbers of embeddings are assigned to each group).
+  - `ndims`: The dimensionality of the embeddings.
+  
+You can halt training early by pressing `Ctrl/Cmd+C`. The mean accuracy and standard error as of the final trial before interruption will be logged if you do this.
+  
+## Explanation
+
+You will notice that if the number of dimensions is a high enough fraction of the number of examples, above chance (i.e., $>\dfrac{ngroups}{100}$) performance on the training set is achieved consistently. Test performance tends to remain at chance (as expected). One should use caution in interpreting the results of fitting an SVM to a few examples of high dimensional data and interpreting the results on the training data. Here, the vectors and groups are randomly generated. A vector encodes no information about its group. Nevertheless, an SVM can be good at distinguishing different random groups if the dimensionality of the embeddings is high enough. In other words, SVMs are good at finding spurious patterns in random data, provided the dimensionality of the data is a high enough fraction of the number of examples.
--- classify-random.py
+++ classify-random.py
@@ -20,7 +20,7 @@ from sklearn.metrics import accuracy_score
 
 log = logging.getLogger(__name__)
 
-def create_randomly_grouped_embeddings(ngroups: int = None, nembeddings: int = None, ndims: int = None) -> Tuple[np.ndarray, List[int]]:
+def create_randomly_grouped_embeddings(ngroups: int, nembeddings: int, ndims: int) -> Tuple[np.ndarray, List[int]]:
 	'''
 	Creates equal-size groups of random embeddings
 	
@@ -42,43 +42,59 @@ def create_randomly_grouped_embeddings(ngroups: int = None, nembeddings: int = N
 	
 	return embeddings, labels
 
-def run_trials(ntrials: int = None, ngroups: int = None, nembeddings: int = None, ndims: int = None) -> None:
+def run_trials(ntrials: int, ngroups: int, ntrain: int, ntest: int, ndims: int) -> None:
 	'''
 	Generates random embeddings and fits a classifier. Logs average accuracy.
 	
 		params:
-			ntrials (int)		: the number of trials to run
-			ngroups (int)			: the number of groups to assign embeddings to
-			nembeddings (int)		: the number of random embeddings ot generate
-			ndims (int)				: the length of the random embeddings
+			ntrials (int)	: the number of trials to run
+			ngroups (int)	: the number of groups to assign embeddings to
+			ntrain (int)	: the number of random embeddings to generate per training set
+			ntest (int) 	: the number of random embeddings to generate per test set
+			ndims (int)		: the length of the random embeddings
 	'''
 	
-	log.info(f'Running {ntrials} trials with ngroups={ngroups}, nembeddings={nembeddings}, ndims={ndims}')
+	log.info(f'Running {ntrials} trials with ngroups={ngroups}, ntrain={ntrain}, ntest={ntest}, ndims={ndims}')
 	
-	accuracies = []
+	train_accuracies 	= []
+	test_accuracies 	= []
 	with logging_redirect_tqdm():
 		try:
 			for i in tqdm(range(ntrials)):
-				inputs, labels 	= create_randomly_grouped_embeddings(ngroups, nembeddings, ndims)
-				classifier 		= svm.SVC(kernel='linear')
-				classifier.fit(inputs, labels)
+				train_inputs, train_labels 	= create_randomly_grouped_embeddings(ngroups, ntrain, ndims)
+				classifier 					= svm.SVC(kernel='linear')
+				classifier.fit(train_inputs, train_labels)
 				
-				accuracy = get_accuracy(classifier, inputs, labels)
-				accuracies.append(accuracy)
-				mean_accuracy = np.mean(accuracies)
+				train_accuracy				 = get_accuracy(classifier, train_inputs, train_labels)
+				train_accuracies.append(train_accuracy)
+				
+				test_inputs, test_labels 	= create_randomly_grouped_embeddings(ngroups, ntest, ndims)
+				test_accuracy 				= get_accuracy(classifier, test_inputs, test_labels)
+				test_accuracies.append(test_accuracy)
+				
+				mean_train_accuracy 		= np.mean(train_accuracy)
+				mean_test_accuracy 			= np.mean(test_accuracies)
+				
 				if i > 0:
-					sem_accuracy  = sem(accuracies)
+					sem_train_accuracy 		= sem(train_accuracies)
+					sem_test_accuracy 		= sem(test_accuracies)
 				else:
-					sem_accuracy = np.nan
+					sem_train_accuracy 		= np.nan
+					sem_test_accuracy 		= np.nan
 				
 				if i % 50 == 49:
-					log.info(f'Mean accuracy at trial {str(len(accuracies)).rjust(len(str(ntrials)))}: {mean_accuracy:.2f} (\u00b1{sem_accuracy:.2f})')
+					log.info(f'Mean train, test accuracy at trial {str(len(train_accuracies)).rjust(len(str(ntrials)))}. Train: {mean_train_accuracy:.2f} (\u00b1{sem_train_accuracy:.2f}), test: {mean_test_accuracy:.2f} (\u00b1{sem_test_accuracy:.2f})')
 				
 		except KeyboardInterrupt:
-			log.warning(f'Halted manually at trial {str(len(accuracies)).rjust(len(str(ntrials)))}')
-			avg_accuracy = np.mean(accuracies)
-			sem_accuracy = sem(accuracies)
-			log.info(f'Mean accuracy at trial {str(len(accuracies)).rjust(len(str(ntrials)))}: {avg_accuracy:.2f} (\u00b1{sem_accuracy:.2f})')
+			log.warning(f'Halted manually at trial {str(len(train_accuracies)).rjust(len(str(ntrials)))}')
+			mean_train_accuracy 			= np.mean(train_accuracies)
+			sem_train_accuracy 				= sem(train_accuracies)
+			
+			# account for cases when we've finished training but not testing at an iteration
+			mean_test_accuracy 				= np.mean(test_accuracies[:len(train_accuracies)])
+			sem_test_accuracy 				= sem(test_accuracies[:len(train_accuracies)])
+			
+			log.info(f'Mean train, test accuracy at trial {str(len(train_accuracies)).rjust(len(str(ntrials)))}. Train: {mean_train_accuracy:.2f} (\u00b1{sem_train_accuracy:.2f}), test: {mean_test_accuracy:.2f} (\u00b1{sem_test_accuracy:.2f})')
 
 def get_accuracy(classifier: svm.SVC, inputs: np.ndarray, labels: List[int]) -> float:
 	'''
@@ -100,7 +116,7 @@ def get_accuracy(classifier: svm.SVC, inputs: np.ndarray, labels: List[int]) -> 
 @hydra.main(config_path='.', config_name='classify-random')
 def main(cfg: DictConfig) -> None:
 	'''Runs trials according to the config file/command line config'''
-	run_trials(cfg.ntrials, cfg.ngroups, cfg.nembeddings, cfg.ndims)
+	run_trials(cfg.ntrials, cfg.ngroups, cfg.ntrain, cfg.ntest, cfg.ndims)
 
 
 if __name__ == '__main__':
